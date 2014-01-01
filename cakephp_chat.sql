f733e82f8ceff5377991800de615b88f1dceaa48
pc2013 <cuongcm.uet@gmail.com>
2014-01-02 01:43:12 +0700

    add sample database

--- cakephp_chat.sql
+++ cakephp_chat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 01, 2014 at 03:18 PM
+-- Generation Time: Jan 01, 2014 at 06:42 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.5.7-1+sury.org~precise+1
 
@@ -27,14 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
-  `message_id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `thread_id` int(10) DEFAULT NULL,
   `user_id` int(10) DEFAULT NULL,
   `deleted` int(1) DEFAULT NULL,
-  PRIMARY KEY (`message_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -44,13 +44,24 @@ CREATE TABLE IF NOT EXISTS `messages` (
 --
 
 CREATE TABLE IF NOT EXISTS `threads` (
-  `thread_id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `threadname` varchar(255) DEFAULT NULL,
   `user_id` int(10) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`thread_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+
+--
+-- Dumping data for table `threads`
+--
+
+INSERT INTO `threads` (`id`, `threadname`, `user_id`, `created`, `updated`) VALUES
+(34, 'admin 2 - created by admin1', 22, '2014-01-01 11:21:21', '2014-01-01 11:21:21'),
+(35, 'test - created by admin1', 22, '2014-01-01 11:21:33', '2014-01-01 11:21:33'),
+(36, 'demo - created by admin', 21, '2014-01-01 11:21:48', '2014-01-01 11:21:48'),
+(37, 'demo1 - created by admin', 21, '2014-01-01 11:21:54', '2014-01-01 11:21:54'),
+(38, 'test - created by test', 25, '2014-01-01 11:34:49', '2014-01-01 11:34:49');
 
 -- --------------------------------------------------------
 
@@ -59,19 +70,22 @@ CREATE TABLE IF NOT EXISTS `threads` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
-(21, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@gmail.com');
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(21, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@gmail.com'),
+(22, 'admin1', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@gmail.com'),
+(24, 'admin3', '33aab3c7f01620cade108f488cfd285c0e62c1ec', 'admin@gmail.com'),
+(25, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'test@gmail.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
